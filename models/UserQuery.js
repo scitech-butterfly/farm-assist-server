f91f69df9efcc766de9ac3142dac73a3b24bb46e
Kashvi Singh <127826843+scitech-butterfly@users.noreply.github.com>
2025-11-06 22:34:33 +0530

    Update UserQuery.js

--- models/UserQuery.js
+++ models/UserQuery.js
@@ -1,16 +1,17 @@
 const mongoose = require("mongoose");
 
-const userQuerySchema = new mongoose.Schema({
-  name: String,
-  age: String,
-  gender: { type: String, enum: ["male", "female", "other"] },
-  crops: String,
-  familySize: String,
-  landSize: String,
-  query: String,
-
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", default: null }
-
-}, { timestamps: true });
+const userQuerySchema = new mongoose.Schema(
+  {
+    name: String,
+    age: String,
+    gender: String,
+    crops: String,
+    familySize: String,
+    landSize: String,
+    query: String,
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", default: null }
+  },
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("UserQuery", userQuerySchema);

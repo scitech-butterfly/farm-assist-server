0fe04582ddd5706ddda1af7b3f69f5651aa94448
Kashvi Singh <127826843+scitech-butterfly@users.noreply.github.com>
2025-11-06 22:07:19 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,51 +1,15 @@
-// models/User.js or models/Query.js
 const mongoose = require('mongoose');
 
-const userQuerySchema = new mongoose.Schema({
-  name: {
-    type: String,
-    trim: true,
-    default: ''
-  },
-  age: {
-    type: String,
-    trim: true,
-    default: ''
-  },
-  gender: {
-    type: String,
-    enum: ['', 'male', 'female', 'other'],
-    default: ''
-  },
-  crops: {
-    type: String,
-    trim: true,
-    default: ''
-  },
-  familySize: {
-    type: String,
-    trim: true,
-    default: ''
-  },
-  landSize: {
-    type: String,
-    trim: true,
-    default: ''
-  },
-  query: {
-    type: String,
-    trim: true
-  },
-  schemes: [{
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Scheme'
-  }],
-  timestamp: {
-    type: Date,
-    default: Date.now
-  }
-}, {
-  timestamps: true
-});
+const userSchema = new mongoose.Schema({
+  name: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
 
-module.exports = mongoose.model('UserQuery', userQuerySchema);
+  age: String,
+  gender: { type: String, enum: ["male", "female", "other"] },
+  crops: String,
+  familySize: String,
+  landSize: String,
+
+}, { timestamps: true });
+
+module.exports = mongoose.model("User", userSchema);

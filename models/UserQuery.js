bfaccc50f65f11de8c05d0912a398bd6b3a6d9cf
Kashvi Singh <127826843+scitech-butterfly@users.noreply.github.com>
2025-11-06 22:06:57 +0530

    Create UserQuery.js

---
+++ models/UserQuery.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const userQuerySchema = new mongoose.Schema({
+  name: String,
+  age: String,
+  gender: { type: String, enum: ["male", "female", "other"] },
+  crops: String,
+  familySize: String,
+  landSize: String,
+  query: String,
+
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", default: null }
+
+}, { timestamps: true });
+
+module.exports = mongoose.model("UserQuery", userQuerySchema);

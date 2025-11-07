7d8bfb11b2a81cd746511130638a60d981a234f0
Kashvi Singh <127826843+scitech-butterfly@users.noreply.github.com>
2025-11-07 14:02:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,12 @@ const feedbackRoutes = require('./routes/feedback');
 const queryRoutes = require('./routes/queryRoutes');
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+}));
+
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
